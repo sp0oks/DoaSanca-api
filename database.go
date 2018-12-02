6e1080151717cc33395ef39fe8a5e34cadf2a547
spooks <g4briel.4lves@gmail.com>
2018-12-02 03:01:12 -0200

    Gorm doesn't like nested structs

--- database.go
+++ database.go
@@ -33,8 +33,8 @@ func setupDB() error {
         db.Create(&User{
                 Name: "Gabriel Alves", 
                 Email: "g4briel.4lves@gmail.com", 
-                Coordinates: Coordinate{Latitude: -22.0027819, 
-                                        Longitude:-47.8970543},
+                Latitude: -22.0027819, 
+                Longitude:-47.8970543,
         })
     }
     if !db.HasTable(&Location{}) {
@@ -43,9 +43,9 @@ func setupDB() error {
                 Name: "Nave Sal da Terra",
                 Type: "Brinquedo",
                 Phone: 1633727823,
-                Address: Address{Street: "R. Dep. Antonio Donato",
-                                 Number: 428,
-                                 Zipcode: 13573560},
+                Street: "R. Dep. Antonio Donato",
+                Number: 428,
+                Zipcode: 13573560,
             })
     }     
     return nil
--- models.go
+++ models.go
@@ -11,16 +11,17 @@ type Coordinate struct {
 
 type Address struct {
     Street      string      `json:"street"`
-    Optional    string      `json:"opt"     gorm:"default:'null'"`
+    Optional    string      `json:"opt"`
     Number      int64       `json:"number"`
     Zipcode     int64       `json:"zipcode"`
 }
 
 type User struct {
     gorm.Model
-    Name        string      `json:"name"    binding:"required"`
-    Email       string      `json:"email"   binding:"required"`
-    Coordinates Coordinate  `json:"coords"`
+    Name        string      `json:"name"      binding:"required"`
+    Email       string      `json:"email"     binding:"required"`
+    Latitude    float64     `json:"latitude"  binding:"required"`
+    Longitude   float64     `json:"longitude" binding:"required"`
 }
     
 type Location struct {
@@ -29,7 +30,10 @@ type Location struct {
     Type        string      `json:"type"    binding:"required"`
     Email       string      `json:"email"   gorm:"default:'null'"`
     Phone       int64       `json:"phone"   binding:"required"`
-    Address     Address     `json:"address"`
+    Street      string      `json:"street"  binding:"required"`
+    Optional    string      `json:"opt"     gorm:"default:'null'"`
+    Number      int64       `json:"number"  binding:"required"`
+    Zipcode     int64       `json:"zipcode" binding:"required"`
     Comment     string      `json:"comment" gorm:"default:'null'"`
     PictureURL  string      `json:"pic_url" gorm:"default:'null'"`
 }
